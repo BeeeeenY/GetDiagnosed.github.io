56ccadcd771ab51ea07327e56ac60aa97951f539
kevanscis <142379221+kevanscis@users.noreply.github.com>
2023-11-04 18:08:09 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -144,65 +144,6 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
 });
 
-function transition() {
-    const box1 = document.querySelector('#newbox');
-    const box2 = document.querySelector('#loginbox')
-    const button = document.getElementById('register');
-    const close = document.getElementById('closebox');
-    const newtext = document.getElementById('newtext')
-    const desc = document.getElementById('description')
-    const username = document.getElementById('username')
-    const confpassword = document.getElementById('confirmpassword')
-    const icon = document.getElementById('togglePassword2')
-    const login = document.getElementById('logintext')
-    const forgotpassword = document.getElementById('forgotpassword')
-    const loginbutton= document.getElementById('loginbutton')
-
-    // Slide the red box to the left (take over the blue box)
-    box1.style.transform = 'translateX(+100%)';
-    box2.style.transform = 'translateX(-100%)';
-    button.style.display = 'none';
-    close.style.display = 'inline';
-    newtext.innerHTML = 'New User?'
-    desc.innerHTML = 'Sign up for quality and easily accessed healthcare'
-    username.placeholder = "Email or Patient ID"
-    confpassword.style.display = 'none'
-    icon.style.display = 'none'
-    login.innerHTML = 'Login'
-    forgotpassword.style.display = 'initial'
-    loginbutton.innerHTML = 'Login'
-}
-function closebox() {
-    const box1 = document.querySelector('#newbox');
-    const box2 = document.querySelector('#loginbox')
-    const button = document.getElementById('register');
-    const close = document.getElementById('closebox');
-    const newtext = document.getElementById('newtext')
-    const desc = document.getElementById('description')
-    const username = document.getElementById('username')
-    const confpassword = document.getElementById('confirmpassword')
-    const icon = document.getElementById('togglePassword2')
-    const login = document.getElementById('logintext')
-    const forgotpassword = document.getElementById('forgotpassword')
-    const loginbutton= document.getElementById('loginbutton')
-    
-
-    // Slide the red box to the left (take over the blue box)
-    box1.style.transform = 'translateX(0)';
-    box2.style.transform = 'translateX(0)';
-    button.style.display = 'inline';
-    close.style.display = 'none';
-    newtext.innerHTML = 'Returning User?'
-    desc.innerHTML = 'Welcome Back! We hope to heal you <3 Click button below to Log In'
-    username.placeholder = "Email"
-    confpassword.style.display = 'initial'
-    icon.style.display = 'initial'
-    login.innerHTML = 'Register'
-    forgotpassword.style.display = 'none'
-    loginbutton.innerHTML = 'Register'
-    
-}
-// END OF PASSWORD FUNCTIONS
 
 
 
